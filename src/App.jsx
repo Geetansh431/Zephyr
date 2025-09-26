96079653f20995889c7ef3666fe96e338ad46b99
geetansh <geetanshg2@gmail.com>
2025-09-26 14:00:19 +0530

    Add About component with GSAP animations and integrate into App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Hero } from "./Components/Hero"
+import { About } from "./Components/About"
 
 export const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero/>
+      <About/>
     </main>
   )
 }
